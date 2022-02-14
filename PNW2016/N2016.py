f17a52e925d16719420a723ab8b0b46d99f81c24
rintrash <90425158+rintrash@users.noreply.github.com>
2022-02-14 04:04:55 -0800

    PNW 2016 N - Barbells Solution

--- PNW2016/N2016.py
+++ PNW2016/N2016.py
@@ -1,43 +1,31 @@
-from itertools import combinations
-def combinePlates(wts):
-        if(len(wts) == 0):
-            return [[]]
+#COMPLETE: Had to look at solution
 
-        #could just add all combinations 
-        
-        combos = []
-        for c in combinePlates(wts[1:]):  
-           combos += [c, c+[wts[0]]]
-
-        return combos
+#gets all possible combinations of plate weights
+from itertools import chain, combinations
+def powerSet(wts):
+    return chain.from_iterable(combinations(wts, r) for r in range(len(wts) + 1))
  
+ #scanner stuff
 str = input().split()
 barbells = int(str[0])
 plates = int(str[1])
-
 str = input().split()
 barbellWts = [int(i) for i in str]
 str = input().split()
 plateWts = [int(i) for i in str]
-#print(plateWts)
-#output different possible distinct weights
-#find all sums of plateWts that are same 
-#i need a set of some sort 
 
-plateCombos = combinePlates(plateWts) 
-plateCombos = [sum(i) for i in plateCombos]
-
-duplicate_sums = set()
-print(plateCombos)
-for s in plateCombos:
-    if plateCombos.count(s) > 1:
-        duplicate_sums.add(s)
-
-print(duplicate_sums)
+duplicate_sums = []
+for i in range(plates):
+    right = set(powerSet(plateWts[i:]))  #ensures they are not taking the same numbers 
+    left = set(powerSet(plateWts[:i]))
+    for leftCombos in left:
+        for rightCombos in right:
+            if sum(leftCombos) == sum(rightCombos):
+                duplicate_sums.append(sum(leftCombos) * 2) #doubled for plates on each side
 
 sumSet = set()
 for b in barbellWts:
     for s in duplicate_sums:
         sumSet.add(b + s)
 
-print(sumSet)       
+print(sorted(sumSet))       
