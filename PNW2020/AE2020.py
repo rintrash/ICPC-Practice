daffa2e21629d82850726303be28aec562446ca4
ANGELA YOU <youa@plu.edu>
2022-03-02 10:29:03 -0800

    Unfinished AE

---
+++ PNW2020/AE2020.py
@@ -0,0 +1,11 @@
+def chooseHouses(roos):
+    #the best houses are at the 25% and 75% lines
+    house1 = roos[len(roos) // 4]
+    house2 = 
+numHouses = 5
+
+roos = [0, 3, -3, 10, 11] 
+roos = sorted(roos)
+
+
+print(roos)
